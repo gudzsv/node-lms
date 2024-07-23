e29b1b3cc921351f1fbbe52e030b8c6e57df6a1e
gudzsv <gudzsvmail@gmail.com>
2024-07-23 15:15:52 +0300

    added mongo DB to prj

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import { initMongoDB } from './db/initMongoDB.js';
 import { startServer } from './server.js';
 
-startServer();
+const bootstrap = async () => {
+	await initMongoDB();
+	startServer();
+};
+
+bootstrap();
--- src/server.js
+++ src/server.js
@@ -4,42 +4,44 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
-const PORT = 3000;
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', 3000));
 
 export const startServer = () => {
-  const app = express();
+	const app = express();
 
-  app.use(express.json());
-  app.use(cors());
+	app.use(express.json());
+	app.use(cors());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+	app.use(
+		pino({
+			transport: {
+				target: 'pino-pretty',
+			},
+		})
+	);
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
-    });
-  });
+	app.get('/', (req, res) => {
+		res.json({
+			message: 'Hello world!',
+		});
+	});
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+	app.use('*', (req, res, next) => {
+		res.status(404).json({
+			message: 'Not found',
+		});
+	});
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+	app.use((err, req, res, next) => {
+		res.status(500).json({
+			message: 'Something went wrong',
+			error: err.message,
+		});
+	});
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+	app.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}`);
+	});
 };
