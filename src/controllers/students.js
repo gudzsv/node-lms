94ecf49cac0af085ba934ff0de3d837a2421723e
gudzsv <gudzsvmail@gmail.com>
2024-08-06 20:23:40 +0300

    added filter to query params

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -8,12 +8,20 @@ import {
 } from '../services/students.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getStudentsController = async (req, res, next) => {
 	const { page, perPage } = parsePaginationParams(req.query);
 	const { sortBy, sortOrder } = parseSortParams(req.query);
+	const filter = parseFilterParams(req.query);
 
-	const students = await getAllStudents({ page, perPage, sortBy, sortOrder });
+	const students = await getAllStudents({
+		page,
+		perPage,
+		sortBy,
+		sortOrder,
+		filter,
+	});
 
 	res.json({
 		status: 200,
--- src/services/students.js
+++ src/services/students.js
@@ -7,20 +7,41 @@ export const getAllStudents = async ({
 	perPage = 10,
 	sortOrder = SORT_ORDER.ASC,
 	sortBy = '_id',
+	filter = {},
 }) => {
 	const limit = perPage;
 	const skip = (page - 1) * perPage;
 
 	const studentsQuery = StudentsCollection.find();
-	const studentsCount = await StudentsCollection.find()
-		.merge(studentsQuery)
-		.countDocuments();
 
-	const students = await studentsQuery
-		.skip(skip)
-		.limit(limit)
-		.sort({ [sortBy]: sortOrder })
-		.exec();
+	if (filter.gender) {
+		studentsQuery.where('gender').equals(filter.gender);
+	}
+
+	if (filter.maxAge) {
+		studentsQuery.where('age').lte(filter.maxAge);
+	}
+
+	if (filter.minAge) {
+		studentsQuery.where('age').gte(filter.minAge);
+	}
+
+	if (filter.maxAvgMark) {
+		studentsQuery.where('avgMark').lte(filter.maxAvgMark);
+	}
+
+	if (filter.minAvgMark) {
+		studentsQuery.where('avgMark').gte(filter(maxAvgMark));
+	}
+
+	const [studentsCount, students] = await Promise.all([
+		StudentsCollection.find().merge(studentsQuery).countDocuments(),
+		studentsQuery
+			.skip(skip)
+			.limit(limit)
+			.sort({ [sortBy]: sortOrder })
+			.exec(),
+	]);
 
 	const paginationData = calculatePaginationData(studentsCount, perPage, page);
 	return { data: students, ...paginationData };
