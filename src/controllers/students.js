629864d73c7d958c5f00f7931f311602ddc58bb1
gudzsv <gudzsvmail@gmail.com>
2024-08-06 16:04:02 +0300

    added pagination data

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -6,9 +6,12 @@ import {
 	getStudentById,
 	updateStudent,
 } from '../services/students.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getStudentsController = async (req, res, next) => {
-	const students = await getAllStudents();
+	const { page, perPage } = parsePaginationParams(req.query);
+
+	const students = await getAllStudents({ page, perPage });
 
 	res.json({
 		status: 200,
