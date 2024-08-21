e50d68994b804ab3f5e189c19c976ca447cb53bc
gudzsv <gudzsvmail@gmail.com>
2024-08-22 00:03:48 +0300

    added save file to cloudinaty and added ENABLE_CLOUDINARY flag to chamge file storage

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,3 +28,9 @@ export const TEMPLATE_FILE_NAME = 'reset-password-email.html';
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+
+export const CLOUDINARY = {
+	CLOUD_NAME: 'CLOUD_NAME',
+	API_KEY: 'API_KEY',
+	API_SECRET: 'API_SECRET',
+};
--- src/controllers/students.js
+++ src/controllers/students.js
@@ -10,6 +10,8 @@ import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
 
 export const getStudentsController = async (req, res, next) => {
 	const { page, perPage } = parsePaginationParams(req.query);
@@ -92,7 +94,11 @@ export const patchStudentController = async (req, res, next) => {
 	let photoUrl;
 
 	if (photo) {
-		photoUrl = await saveFileToUploadDir(photo);
+		if (env('ENABLE_CLOUDINARY') === 'true') {
+			photoUrl = await saveFileToCloudinary(photo);
+		} else {
+			photoUrl = await saveFileToUploadDir(photo);
+		}
 	}
 
 	const result = await updateStudent(studentId, {
