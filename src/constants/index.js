83fed66baba59ca5cc8d0f65d6ee483b3b309675
gudzsv <gudzsvmail@gmail.com>
2024-08-21 19:04:53 +0300

    added multer middleware and createDirIfNotExists utils

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,3 +24,7 @@ export const SMTP = {
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 
 export const TEMPLATE_FILE_NAME = 'reset-password-email.html';
+
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+
+export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
---
+++ src/utils/createDirIfNotExists.js
@@ -0,0 +1,11 @@
+import { fs } from 'node:fs/promises';
+
+export const createDirIfNotExists = async (url) => {
+	try {
+		await fs.access(url);
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			await fs.mkdir(url);
+		}
+	}
+};
